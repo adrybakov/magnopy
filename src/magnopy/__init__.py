520ccdb5c595a84cde631636c0a150d45b4c646b
Andrey Rybakov <rybakov.ad@icloud.com>
2026-03-23 14:44:58 +0100

    Update metadata

--- src/magnopy/__init__.py
+++ src/magnopy/__init__.py
@@ -20,9 +20,9 @@
 # ================================ END LICENSE =================================
 
 
-__version__ = "0.4.3"
+__version__ = "0.5.0.dev"
 __doclink__ = "magnopy.org"
-__release_date__ = "9 January 2026"
+__release_date__ = "Undefined"
 
 from . import examples, io, scenarios, experimental
 from ._constants import _si as si
